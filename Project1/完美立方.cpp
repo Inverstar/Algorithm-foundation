db73fef9116e6447e377c786ff497c3414dbb7a3
Inverstar <lnq1208@163.com>
2021-09-28 00:36:06 +0800

    新建项目与解决方案
    Project与Project1

---
+++ Project1/完美立方.cpp
@@ -0,0 +1,18 @@
+﻿#include	<iostream>
+#include	<cstdio>
+using namespace std;
+
+int 完美立方()
+{
+	int N = 0;
+	cin >> N;
+	for (int a = 2; a <= N; ++a)
+		for (int b = 2; b < a; ++b)
+			for (int c = b; c < a; ++c)
+				for (int d = c; d < a; ++d)
+					if (a * a * a == b * b * b + c * c * c + d * d * d)
+						cout << "Cube = " << a << "," << "Triple = " 
+						<< "(" << b << "," << c << "," << d << ")" << endl;
+
+	return 0;
+}
---
+++ Project1/源.cpp
@@ -0,0 +1,10 @@
+﻿#include	<iostream>
+#include	<cstdio>
+using namespace std;
+
+int main()
+{
+	int 完美立方();
+	完美立方();
+	return 0;
+}
